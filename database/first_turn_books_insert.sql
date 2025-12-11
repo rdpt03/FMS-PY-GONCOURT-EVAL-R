8feec7f08aa7e8bdc6ef07d53c704213a79dee91
Rafael DA SILVA <rafael1893porto@gmail.com>
2025-12-11 23:45:56 +0100

    add inserts to vote table

--- database/first_turn_books_insert.sql
+++ database/first_turn_books_insert.sql
@@ -1,4 +1,4 @@
-INSERT INTO session (selection_date, voting,session_name) VALUES ("2025-09-03",FALSE,"Premiere sélection du prix Goncourt 2025");
+INSERT INTO session (selection_date, voting,session_name, n_of_winners, session_n) VALUES ("2025-09-03",FALSE,"Premiere sélection du prix Goncourt 2025",8,1);
 
 INSERT INTO session_have_book (id_session,id_book) VALUES 
 	(1,1),
@@ -16,4 +16,8 @@ INSERT INTO session_have_book (id_session,id_book) VALUES
     (1,13),
     (1,14),
     (1,15);
+
+-- votes
+insert into vote values (1,NULL,15,1),(2,NULL,14,1),(3,NULL,10,1),(4,NULL,6,1),(5,NULL,9,1),(6,NULL,5,1),(7,NULL,13,1),(8,NULL,1,1);
+insert into vote values (9,NULL,15,1),(10,NULL,14,1),(11,NULL,10,1),(12,NULL,6,1),(13,NULL,9,1),(14,NULL,5,1),(15,NULL,13,1),(16,NULL,1,1),(17,NULL,11,1),(18,NULL,2,1);
     
