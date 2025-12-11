09d14cfa424c532e7177a23f2f3233ff549e9cb3
Rafael DA SILVA <rafael1893porto@gmail.com>
2025-12-11 15:44:15 +0100

    Add get books and votes from this DAO Add id to session oop after creation add extra spaces between functions add get_session_books and get_session_votes to link with the book daos and vote dao to get the books of this session and votes of this sessions

--- daos/session_dao.py
+++ daos/session_dao.py
@@ -1,7 +1,10 @@
-from typing import Optional, List
+from typing import Optional, List, TYPE_CHECKING
 
+from daos.book_dao import BookDao
 from daos.dao import Dao
+from models.book import Book
 from models.session import Session
+from models.vote import Vote
 
 
 class SessionDao(Dao[Session]):
@@ -16,6 +19,11 @@ class SessionDao(Dao[Session]):
                 voting=session_sql['voting']
             )
             oop_session.id = session_sql['id_session']
+            #get his books
+            oop_session.books = self.get_session_books(oop_session)
+
+            #get votes
+            oop_session.votes = self.get_session_votes(oop_session)
         return oop_session
 
     def create(self, session: Session) -> int:
@@ -28,7 +36,9 @@ class SessionDao(Dao[Session]):
             sql = "INSERT INTO session (session_name, selection_date, voting) VALUES (%s, %s, %s);"
             cursor.execute(sql, (session.session_name, session.selection_date, session.voting))
             Dao.connection.commit()
-            return cursor.lastrowid
+            session.id = cursor.lastrowid
+        return session.id
+
 
     def read(self, id_session: int) -> Optional[Session]:
         """Return the Session object for the given id, or None"""
@@ -37,6 +47,7 @@ class SessionDao(Dao[Session]):
             cursor.execute(sql, (id_session,))
             session_sql = cursor.fetchone()
         return self.session_sql_to_oop(session_sql)
+
 
     def read_all(self) -> List[Session]:
         """Return a list of all Session objects"""
@@ -50,6 +61,7 @@ class SessionDao(Dao[Session]):
                     oop_list.append(self.session_sql_to_oop(sess_sql))
         return oop_list
 
+
     def update(self, session: Session) -> bool:
         """Update the Session in the database"""
         with Dao.connection.cursor() as cursor:
@@ -58,6 +70,7 @@ class SessionDao(Dao[Session]):
             Dao.connection.commit()
             return cursor.rowcount > 0
 
+
     def delete(self, session: Session) -> bool:
         """Delete the Session from the database"""
         with Dao.connection.cursor() as cursor:
@@ -65,3 +78,16 @@ class SessionDao(Dao[Session]):
             cursor.execute(sql, (session.id,))
             Dao.connection.commit()
             return cursor.rowcount > 0
+
+
+    def get_session_books(self, session : Session) -> List[Book]:
+        return BookDao().read_by_session_id(session.id)
+
+    def get_session_votes(self, session : Session) -> List[Vote]:
+        from daos.vote_dao import VoteDao
+        #get votes
+        votes = VoteDao().read_all_from_session(session.id)
+        #assign this session
+        for vote in votes:
+            vote.session = session
+        return votes
