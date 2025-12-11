6fc6148505c732dca82a46ccb1f6767f2de04297
Rafael DA SILVA <rafael1893porto@gmail.com>
2025-12-11 23:51:42 +0100

    add use of main_menu class to show the main menu

--- main.py
+++ main.py
@@ -5,8 +5,7 @@
 Application de gestion d'une école
 """
 from business.goncourt import Goncourt
-from daos.book_dao import BookDao
-from view import invited_view
+from view import main_view
 
 
 def main() -> None:
@@ -20,29 +19,7 @@ Bienvenue dans le logiciel du Goncourt
     goncourt = Goncourt()
 
     menu_main = True
-    while menu_main:
-        #initial
-        print("---> Se connecter comme \n"
-              "1 - Invité\n"
-              "2 - Jury [pas encore implementé]\n"
-              "3 - President\n")
-        chosen_option = input("Choisir une option -> ")
-
-        #option 1 for invited
-        if chosen_option == "1":
-            invited_view.invited_menu_choose_option(goncourt)
-
-        #option 2 for jury
-        elif chosen_option == "2":
-            print("Fonctionnalité pas implementé!")
-
-        #option 3 for director
-        elif chosen_option == "3":
-            ...
-
-        #default
-        else:
-            print("Option non valide!")
+    main_view.menu_main(goncourt)
 
 if __name__ == '__main__':
     main()
