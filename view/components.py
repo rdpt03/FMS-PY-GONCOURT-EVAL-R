20210e0ff0a9233ea8530cb062d5e9ff72b93a97
Rafael DA SILVA <rafael1893porto@gmail.com>
2025-12-12 10:50:14 +0100

    Add view functions to properly ask int and date Add function to ask date, and properly convert to type date or show error Add function to ask int, and properly convert to type int or show error

--- view/components.py
+++ view/components.py
@@ -1,2 +1,40 @@
+from datetime import datetime
+from typing import List
+
+
 def barrier(text = ""):
     print(('-'*30) + text + ('-'*30))
+
+
+def ask_date(msg : str):
+    while True:
+        date_str = input(f"{msg} [Format souhaité : jj/mm/aaaa] ")
+        date_split = date_str.split("/")
+
+        if len(date_split) != 3:
+            print("Format de la date incorrecte")
+            continue
+
+        try:
+            # Conversion correcte : jour, mois, année
+            date = datetime(int(date_split[2]), int(date_split[1]), int(date_split[0]))
+            return date
+        except ValueError:
+            print("Format de la date incorrecte")
+
+def ask_int(msg : str, desired_numbers : List[int] = None):
+    while True:
+        #ask number
+        int_str = input(f'{msg} [Inserer uniquement des numeros]')
+
+        #convert to int
+        if int_str.isdigit():
+            #check if desired number
+            if desired_numbers:
+                if int(int_str) in desired_numbers:
+                    return int(int_str)
+            #no given desired numbers
+            else:
+                return int(int_str)
+        else:
+            print('Numero non valide')
